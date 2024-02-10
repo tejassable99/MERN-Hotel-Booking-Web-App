9cb3fe300d488cd4ab3b7618897e71bc7a612656
tejas <tejassable07@gmail.com>
2024-02-10 12:02:44 +0530

    alert msg

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,7 @@ const dotenv=require("dotenv")
 dotenv.config({path:"./config.env"})
 const port=process.env.port;
 app.use(express.json())
-app.use(cors({origin:'https://mern-hotel-booking-web-app-s-git-76056b-tejas-projects-c8790610.vercel.app/'}))
+app.use(cors({origin:'https://mern-hotel-booking-web-app-s7y7.vercel.app'}))
 app.use(route)
 
 app.listen(port,()=>
