c50105d85a27a478e07e9500c99f132ab60f6e5c
tejas <tejassable07@gmail.com>
2024-02-10 12:32:31 +0530

    cors

--- Server/app.js
+++ Server/app.js
@@ -6,10 +6,12 @@ const dotenv=require("dotenv")
 dotenv.config({path:"./config.env"})
 const port=process.env.port;
 app.use(express.json())
-app.use(cors({origin:'https://mern-hotel-booking-web-app-s7y7.vercel.app'}))
+app.use(cors({
+origin:'https://mern-hotel-booking-web-app-w17x.vercel.app/'}))
 app.use(route)
 
 app.listen(port,()=>
 {
     console.log(`server on ${port}`)
 })
+// origin:'https://mern-hotel-booking-web-app-s7y7.vercel.app'
